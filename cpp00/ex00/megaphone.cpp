80f0e9c010bce0ad48bde9746960745d3fc92b19
Jingyi Tu <jtu@c1r5p6.hive.fi>
2024-07-03 15:26:57 +0300

    rename cpp00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtu <jtu@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/30 17:12:13 by jtu               #+#    #+#             */
+/*   Updated: 2024/07/03 15:25:31 by jtu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+	
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		i = 0;
+		while (argv[++i])
+		{
+			j = -1;
+			while (argv[i][++j])
+				std::cout << (char)toupper(argv[i][j]) ;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
