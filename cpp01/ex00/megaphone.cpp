98ceec3bd90513052c5fa0a56efd91f399a13e33
Jingyi Tu <jtu@c1r5p6.hive.fi>
2024-07-03 14:07:37 +0300

    edit Makefile and finish cpp function

--- cpp01/ex00/megaphone.cpp
+++ cpp01/ex00/megaphone.cpp
@@ -3,19 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jtu <jtu@student.hive.fi>                  +#+  +:+       +#+        */
+/*   By: jtu <jtu@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 17:12:13 by jtu               #+#    #+#             */
-/*   Updated: 2024/05/30 17:24:19 by jtu              ###   ########.fr       */
+/*   Updated: 2024/07/02 15:56:19 by jtu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
 int main(int argc, char *argv[])
 {
+	int	i;
+	int	j;
+	
 	if (argc == 1)
-		std::cout
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
-
+		i = 0;
+		while (argv[++i])
+		{
+			j = -1;
+			while (argv[i][++j])
+				std::cout << (char)toupper(argv[i][j]) ;
+		}
 	}
+	std::cout << std::endl;
+	return (0);
 }
