1c2d362d89f4a4302e79963e8439c731fa91bcc7
Leonor Tu <Jingyi.Tu@outlook.com>
2025-02-18 20:29:08 +0200

    feat: complete iter

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -5,16 +5,24 @@ int main()
 {
 
   std::cout << "==== TEST 1 ====" << std::endl;
-  int num1 = 42;
-	int num2 = -5;
-	std::cout << "before swap: num1 = " << num1 << ", num2 = " << num2<< std::endl;
-	::swap( num1, num2);
-	std::cout << "after swap: num1 = " << num1 << ", num2 = " << num2<< std::endl;
-	std::cout << "min( num1, num2) = " << ::min( num1, num2) << std::endl;
-	std::cout << "max( num1, num2) = " << ::max( num1, num2) << std::endl;
-
+  int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	
 	std::cout << std::endl;
 	std::cout << "==== TEST 2 ====" << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "==== TEST 3 ====" << std::endl;
   char c1 = 'A';
 	char c2 = 'a';
 	std::cout << "before swap: c1 = " << c1 << ", c2 = " << c2 << std::endl;
@@ -24,7 +32,7 @@ int main()
 	std::cout << "max( c1, c2 ) = " << ::max( c1, c2 ) << std::endl;
 
 	std::cout << std::endl;
-	std::cout << "==== TEST 3 ====" << std::endl;
+	std::cout << "==== TEST 4 ====" << std::endl;
   std::string str1 = "Hello";
 	std::string str2 = "Hell";
 	std::cout << "before swap: str1 = " << str1 << ", str2 = " << str2 << std::endl;
@@ -34,7 +42,7 @@ int main()
 	std::cout << "max( str1, str2 ) = " << ::max( str1, str2 ) << std::endl;
 
 	std::cout << std::endl;
-	std::cout << "==== TEST 4 ====" << std::endl;
+	std::cout << "==== TEST 5 ====" << std::endl;
   float num3 = 42.2;
 	float num4 = 42.3;
 	std::cout << "before swap: num3 = " << num3 << ", num4 = " << num4<< std::endl;
